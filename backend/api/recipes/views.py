d02464aa5a5a9d0d6074a4d2c571e6da97bed5e5
GalinaLody <lodyginagl@gmail.com>
2026-04-08 12:09:04 +0300

    исправила редирект короткой ссылки 2

--- backend/api/recipes/views.py
+++ backend/api/recipes/views.py
@@ -241,4 +241,4 @@ def redirect_to_recipe_url(request, short_link: str):
     """При получении короткой ссылки рецепта перенаправляет пользователя
     на страницу рецепта."""
     recipe = get_object_or_404(Recipe, short_link=short_link)
-    return redirect(f'recipes/{recipe.id}')
+    return redirect(recipe)
